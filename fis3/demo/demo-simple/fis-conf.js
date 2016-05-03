0a3e14010017958e51c4fee574a2067c9cbf7219
Cleam Lee <lizhigao1231@163.com>
2016-05-03 17:47:17 +0800

    fis3 study

--- fis3/demo/demo-simple/fis-conf.js
+++ fis3/demo/demo-simple/fis-conf.js
@@ -18,3 +18,25 @@
 // fis.match('*.png', {
 //   optimizer: fis.plugin('png-compressor')
 // });
+// 
+
+// 配置配置文件，注意，清空所有的配置，只留下以下代码即可。
+fis.match('*.{png,js,css}', {
+  release: '/static/$0'
+});
+
+fis.match('*.js', {
+  useHash: true
+});
+
+fis.match('*.css', {
+  useHash: true
+});
+
+fis.match('*.png', {
+  useHash: true
+});
+
+
+
+ 
