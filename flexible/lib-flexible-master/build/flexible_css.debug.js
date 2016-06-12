ffde1918332fe3e852ba0d934e416a4c0b57d7d7
Cleam Lee <lizhigao1231@163.com>
2016-06-12 20:58:57 +0800

    update flexible

--- flexible/lib-flexible-master/build/flexible_css.debug.js
+++ flexible/lib-flexible-master/build/flexible_css.debug.js
@@ -7,9 +7,9 @@
     var styleEl = document.createElement("style");
     document.getElementsByTagName("head")[0].appendChild(styleEl);
     if (styleEl.styleSheet) {
-        if (!styleEl.styleSheet.disabled) {
+        if (!styleEl.styleSheet.disabled) 
             styleEl.styleSheet.cssText = cssText;
-        }
+        
     } else {
         try {
             styleEl.innerHTML = cssText
